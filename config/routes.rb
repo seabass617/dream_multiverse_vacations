1a8fe39b1df3a4d9d3070903bfebcb5505f25578
Sebastian Viasus <vseb85@gmail.com>
2020-09-03 17:03:16 -0400

    Listings Index HTML + Basic CSS complete, delete path needs to be updated

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:edit, :show, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :dimensions, only: [ :index, :show, :new, :create] do
+  resources :dimensions, only: [ :index, :show, :new, :create, :edit, :update, :destroy] do
     resources :bookings, only: [:new, :create]
   end
   resources :bookings, only: [:index, :destroy]   
